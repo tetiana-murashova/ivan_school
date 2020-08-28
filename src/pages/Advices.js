7c9f1b460ff912cc6ed9f3128b2a1abe6cfd6839
TetianaMurashova <murashovatania1@gmail.com>
2020-08-28 13:20:15 +1000

    adv

--- src/pages/Advices.js
+++ src/pages/Advices.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Advices = () => {
     return (
         <div>
-            <p className='display-2'>рассписание</p>
+            <p className='display-2'>расписание</p>
 
         </div>
     );
